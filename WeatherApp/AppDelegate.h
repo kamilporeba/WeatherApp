7dbe320999bcc924e725a80efef745de95507538
Kamil Poręba <kamil.poreba@icloud.com>
2016-10-16 18:17:18 +0200

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Kamil Poręba on 16.10.2016.
+//  Copyright © 2016 Kamil Poręba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Kamil Poręba on 16.10.2016.
+//  Copyright © 2016 Kamil Poręba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
